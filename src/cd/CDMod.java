f2045b8b96bb3370b505fa322991de292ddfe6ba
woodjecket <1164806786@qq.com>
2023-01-02 09:22:44 +0800

    Some commits

--- src/cd/CDMod.java
+++ src/cd/CDMod.java
@@ -9,7 +9,7 @@ import mindustry.ui.dialogs.*;
 public class CDMod extends Mod{
 
     public CDMod(){
-        Log.info("Loaded ExampleJavaMod constructor.");
+        Log.info("Loaded CDMod constructor.");
 
         //listen for game load event
         Events.on(ClientLoadEvent.class, e -> {
@@ -18,7 +18,7 @@ public class CDMod extends Mod{
                 BaseDialog dialog = new BaseDialog("frog");
                 dialog.cont.add("behold").row();
                 //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
+                dialog.cont.image(Core.atlas.find("commumdustry-frog")).pad(20f).row();
                 dialog.cont.button("I see", dialog::hide).size(100f, 50f);
                 dialog.show();
             });
