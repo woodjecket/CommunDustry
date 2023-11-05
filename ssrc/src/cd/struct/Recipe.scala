9cd61cb13db7b3e87538643bd78c8d9b2385a9af
woodjecket <1164806786@qq.com>
2023-11-05 17:59:02 +0800

    Attempt for a new organization of recipe (2nd part)

--- ssrc/src/cd/struct/Recipe.scala
+++ ssrc/src/cd/struct/Recipe.scala
@@ -1,26 +1,72 @@
 package cd.struct
 
 import arc.scene.ui.layout.Table
-import cd.struct.CDCraft.{CDCondition, CDConsume, CDProduce}
+import cd.struct.CDCraft.{CDCondition, CDConsume, CDProduce, CDRecipePart}
+import cd.util.SAMConversation.{lamdba2Cons, lamdba2Floatp, lamdba2Prov}
 import mindustry.ctype.UnlockableContent
-import mindustry.gen.Building
+import mindustry.gen.{Building, Icon}
 
-case class Recipe(consume: CDConsume, produce: CDProduce, condition: CDCondition, iconItem: UnlockableContent, craftTime : Float) {
-  def buildTable(build: Building, table: Table): Table = condition.buildTable(build, produce.buildTable(build, consume.buildTable(build, table)))
+case class Recipe(consume: CDConsume, produce: CDProduce,
+                  condition: CDCondition, iconItem: UnlockableContent, craftTime: Float) extends
+  Iterator[CDRecipePart] {
+  val serialVersionUID: Long = 732473L
+  private[this] var ordinal = -1
 
 
+  override def hasNext: Boolean = ordinal > 2
+
+  override def isTraversableAgain: Boolean = true
+
+  override def next(): CDRecipePart = {
+    ordinal += 1
+    ordinal match {
+      case 0 => consume
+      case 1 => produce
+      case 2 => condition
+      case 3 => ordinal -= 3; consume
+    }
+  }
+
+  def genReactionTable(): Table = new Table((t: Table) => {
+    //Reactants' table
+    t.add(consume.genReaction())
+
+    //Conditions' table
+    val conditionTable = new Table((ct: Table) => {
+      ct.table((up: Table) => {
+        up.add(condition.genReaction(): _*)
+      }).row()
+      ct.table((arrow: Table) => {
+        //TODO Automatic check
+        val lineNum: Int = 7
+        for (i <- 1 to lineNum ) {
+          arrow.image(Icon.lineSmall)
+        }
+        arrow.image(Icon.rightSmall)
+        ()
+      }).row()
+      ct.add(new Table()).row()
+    })
+    t.add(conditionTable)
+
+    //Products' table
+    t.add(produce.genReaction());()
+  }
+  )
 }
 
 object Recipe {
   case class RecipeEntity(recipe: Recipe) {
-    def craft(building: Building) = {
+    val serialVersionUID: Long = 732473368489L
+
+    def craft(building: Building): Unit = {
       recipe.consume.triggerOnce(building)
       recipe.produce.triggerOnce(building)
     }
 
-    def triggerPerTick(building: Building) = {
-      recipe.consume.triggerPerTick(building,efficiency)
-      recipe.produce.triggerPerTick(building,efficiency)
+    def triggerPerTick(building: Building): Unit = {
+      recipe.consume.triggerPerTick(building, efficiency)
+      recipe.produce.triggerPerTick(building, efficiency)
       progress += 1 / recipe.craftTime
     }
 
--- ssrc/src/cd/world/component/CDBaseCrafter.scala
+++ ssrc/src/cd/world/component/CDBaseCrafter.scala
@@ -1,13 +1,18 @@
 package cd.world.component
 
 
+import arc.Core
+import arc.scene.ui.Button
+import arc.scene.ui.layout.Table
 import arc.struct.EnumSet
 import cd.struct.CDCraft.{CDCondition, CDConsume, CDProduce}
 import cd.struct.Recipe
 import cd.struct.Recipe.RecipeEntity
+import cd.util.SAMConversation.{lamdba2Cons, lamdba2Runnable}
 import cd.world.component.CDComp.{CDBlockComp, CDBuildingComp}
 import mindustry.content.Items
-import mindustry.gen.Sounds
+import mindustry.gen.{Sounds, Tex}
+import mindustry.ui.Styles
 import mindustry.world.meta.BlockFlag
 
 import scala.collection.mutable.ArrayBuffer
@@ -21,42 +26,62 @@ trait CDBaseCrafter extends CDBlockComp {
   ambientSoundVolume = 0.03f
   flags = EnumSet.of(BlockFlag.factory)
   drawArrow = false
-
+  
+  configurable = true
+  
   val recipes: ArrayBuffer[Recipe] = new ArrayBuffer[Recipe]()
-
-  def addRecipe(consume:CDConsume, product:CDProduce, condition: CDCondition, craftTime:Float):Unit =
+  
+  
+  def addRecipe(consume: CDConsume, product: CDProduce, condition: CDCondition, craftTime: Float): Unit =
     recipes += new Recipe(consume = consume, produce = product, condition = condition, iconItem = Items.copper, craftTime = craftTime)
-
+  
   override def init(): Unit = {
     super.init()
-    recipes.foreach(r=>{
-      r.produce.init(this)
-      r.consume.init(this)
-      r.condition.init(this)
-    })
+    recipes.map(_.map(_.init(this)))
   }
-
+  
   trait CDBaseCrafterBuild extends CDBuildingComp {
     //Wonderful grammar that the case class is also a producer function
-    val recipeEntity: ArrayBuffer[RecipeEntity] = recipes.map(RecipeEntity)
-    @inline private[this] def applyToAllRecipeEntities[A](f: RecipeEntity => A): ArrayBuffer[A] = {
-      recipeEntity.map(f)
+    private[this] val recipeEntity: ArrayBuffer[RecipeEntity] = recipes.map(RecipeEntity)
+    
+    override def buildConfiguration(table: Table): Unit = {
+      
+      table.table(Tex.buttonTrans, (t: Table) => {
+        t.defaults.grow.marginTop(0).marginBottom(0).marginRight(5).marginRight(5)
+        t.add(Core.bundle.get("select-recipe")).padLeft(5).padTop(5).padBottom(5)
+          .row()
+        t.pane((buttons: Table) => {
+          recipeEntity.foreach(r => {
+            buttons.button(
+              (b: Button) => {
+                b.add(r.recipe.genReactionTable())
+              },
+              Styles.underlineb, () => r.enabled = !r.enabled
+            ).update((b: Button) => b.setChecked(r.enabled))
+              .fillY.growX().left().margin(5).marginTop(8).marginBottom(8).pad(4)
+              .row()
+          })
+        }).fill().maxHeight(280f);
+      })
+      
+      table.row()
     }
+    
     override def updateTile(): Unit = {
       super.updateTile()
-      applyToAllRecipeEntities { recipeEntity =>
-        if (recipeEntity.sufficient(this)) {
+      recipeEntity.foreach { recipeEntity =>
+        if(recipeEntity.sufficient(this)) {
           recipeEntity.triggerPerTick(this)
-          if (recipeEntity.progress >= 1) {
+          if(recipeEntity.progress >= 1) {
             recipeEntity.craft(this)
             recipeEntity.progress %= 1
           }
         }
       }
     }
-
+    
     override def shouldConsume(): Boolean =
-      applyToAllRecipeEntities { _.recipe.produce.canProduce(this) }.reduce(_ || _)
-
+      recipeEntity.map { _.recipe.produce.canProduce(this) }.reduce(_ || _)
+    
   }
 }
