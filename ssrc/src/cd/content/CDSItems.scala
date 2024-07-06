4725ad8a81de88b9082d69c76be7a17c55440f4f
Woodjecket <114514@>
2024-07-06 21:40:54 +0800

    Cleanup

--- ssrc/src/cd/content/CDSItems.scala
+++
@@ -1,7 +0,0 @@
-package cd.content
-
-import cd.struct.meta.Meta
-
-object CDSItems extends Applyable{
-  val iron = Meta.meta("iron").ingot
-}
--- ssrc/src/cd/util/SLog.scala
+++
@@ -1,9 +0,0 @@
-package cd.util
-
-import arc.util.Log
-
-object SLog {
-  def info(xs : String ):Unit = {
-    Log.info(xs.asInstanceOf[AnyRef])
-  }
-}
