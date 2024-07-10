39015e755d8b18c44e0036126400d742cd4d069a
Woodjecket <114514@>
2024-07-10 20:15:59 +0800

    Fix [CIP-1] (Wall ore and player are WIP)

--- src/cd/entities/FiniteOreUpdater.java
+++ src/cd/entities/FiniteOreUpdater.java
@@ -4,6 +4,7 @@ import arc.Events;
 import arc.math.Mathf;
 import arc.struct.ObjectIntMap;
 import arc.struct.Seq;
+import arc.util.Log;
 import cd.io.FiniteOreCustomChunk;
 import cd.world.block.environment.Finite;
 import mindustry.Vars;
@@ -17,43 +18,47 @@ import mindustry.world.blocks.production.Drill;
 import mindustry.world.meta.BlockFlag;
 
 public class FiniteOreUpdater {
-    private final Runnable _update = this::update;
+    public static final int DEFAULT_ORE_AMOUNT = 100;
     @SuppressWarnings("unused")
-    private final  FiniteOreCustomChunk focc = new FiniteOreCustomChunk();
+    private final FiniteOreCustomChunk focc = new FiniteOreCustomChunk();
 
     public final ObjectIntMap<Tile> amountMap = new ObjectIntMap<>();
 
     private final Seq<Building> drills = new Seq<>();
     private final Seq<Tile> tmpTileSeq = new Seq<>(36);
+    ;
 
     public void update() {
-        for (Building building : drills) {
-            if(building instanceof Drill.DrillBuild drill && !Vars.state.isPaused()){
-                var progressD = 0f ;
-                //emulate progress
-                if(drill.items.total() < drill.block.itemCapacity && drill.dominantItems > 0 && drill.efficiency > 0){
-                    float speed = Mathf.lerp(1f, ((Drill) drill.block).liquidBoostIntensity, drill.optionalEfficiency) * drill.efficiency;
-                    var wup = Mathf.approachDelta(drill.warmup, speed, ((Drill) drill.block).warmupSpeed);
-                    progressD = drill.delta() *drill. dominantItems * speed * wup;
-                }
-                if (drill.dominantItems > 0 && drill.progress + progressD >= ((Drill) drill.block).getDrillTime(drill.dominantItem)
-                        && drill.items.total() < drill.block.itemCapacity) {
-                    //offload(dominantItem);
-                    consumeGroundOnce(drill);
+        if (!Vars.state.isPaused())
+            for (Building building : drills) {
+                if (building instanceof Drill.DrillBuild drill) {
+                    if (drill.dominantItems > 0 && drill.progress + emulateDrillProgress(drill) >= ((Drill) drill.block).getDrillTime(drill.dominantItem) && drill.items.total() < drill.block.itemCapacity) {
+                        consumeGroundOnce(drill);
+                    }
                 }
             }
+    }
+
+    //emulate progress
+    private static float emulateDrillProgress(Drill.DrillBuild drill) {
+        if (drill.items.total() < drill.block.itemCapacity && drill.dominantItems > 0 && drill.efficiency > 0) {
+            float speed = Mathf.lerp(1f, ((Drill) drill.block).liquidBoostIntensity, drill.optionalEfficiency) * drill.efficiency;
+            var wup = Mathf.approachDelta(drill.warmup, speed, ((Drill) drill.block).warmupSpeed);
+            return drill.delta() * drill.dominantItems * speed * wup;
         }
+        return 0f;
     }
 
     private void consumeGroundOnce(Drill.DrillBuild drill) {
         tmpTileSeq.clear();
-        drill.tile.getLinkedTilesAs(drill.block,tmpTileSeq);
-        tmpTileSeq.retainAll(t->t.drop() == drill.dominantItem);
-        if(tmpTileSeq.contains(this::isInfiniteFloor)) return;
-        tmpTileSeq.sort((t1,t2)->amountMap.get(t2) - amountMap.get(t1));
+        drill.tile.getLinkedTilesAs(drill.block, tmpTileSeq);
+        tmpTileSeq.retainAll(t -> t.drop() == drill.dominantItem);
+        if (tmpTileSeq.contains(this::isInfiniteFloor)) return;
+        tmpTileSeq.sort((t1, t2) -> amountMap.get(t2, DEFAULT_ORE_AMOUNT) - amountMap.get(t1,DEFAULT_ORE_AMOUNT));
+        Log.infoList(tmpTileSeq,amountMap);
         var first = tmpTileSeq.first();
-        amountMap.increment(first,99,-1);
-        if(amountMap.get(first) < 1){
+        amountMap.increment(first, DEFAULT_ORE_AMOUNT, -1);
+        if (amountMap.get(first) < 1) {
             exhaust(first);
         }
     }
@@ -66,11 +71,9 @@ public class FiniteOreUpdater {
         return !(dropFloor(tile) instanceof Finite);
     }
 
-    private Floor dropFloor(Tile t){
+    private Floor dropFloor(Tile t) {
         return t.overlay() == Blocks.air || t.overlay().itemDrop == null ? t.floor() : t.overlay();
     }
-
-
     public void worldChange() {
         drills.clear();
         for (Team team : Team.all) {
@@ -80,8 +83,8 @@ public class FiniteOreUpdater {
     }
 
     public void init() {
-        Events.run(EventType.Trigger.update, _update);
-        Events.run(EventType.BlockBuildEndEvent.class,this::worldChange);
+        Events.run(EventType.Trigger.update, this::update);
+        Events.run(EventType.BlockBuildEndEvent.class, this::worldChange);
     }
 
 }
