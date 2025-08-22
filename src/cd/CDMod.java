c8e60ee7e4e5595ab25097f083907b06c2677416
woodjecket <1164806786@qq.com>
2025-08-22 12:19:35 +0800

    Cleanup

--- src/cd/CDMod.java
+++ src/cd/CDMod.java
@@ -6,16 +6,14 @@ import arc.scene.ui.layout.*;
 import arc.util.*;
 import cd.manager.*;
 import cd.ui.*;
-import cd.world.block.*;
 import mindustry.*;
 import mindustry.mod.*;
-import mindustry.type.*;
-import mindustry.world.*;
 
 import static mindustry.Vars.state;
 
 
 public class CDMod extends Mod{
+
     public WidgetGroup galGroup = new WidgetGroup();
 
 
@@ -27,19 +25,20 @@ public class CDMod extends Mod{
     @Override
     public void init(){
         super.init();
-        Vars.ui.hudGroup.visible(() -> state.isGame() && !GalManager.visible);
-        galGroup.fillParent = true;
-        galGroup.touchable = Touchable.childrenOnly;
-        galGroup.visible(() ->state.isGame() && GalManager.visible);
-        Core.scene.add(galGroup);
-        GalFragment.build(galGroup);
+        initUI();
     }
 
     @Override
     public void loadContent(){
-        Log.info("Cosmos insight benefits well-being");
-        Block test = new TestGalBlock("test"){{
-            requirements(Category.crafting,new ItemStack[]{});
-        }};
+        Log.info("All is Alon Master's order");
+    }
+
+    private void initUI(){
+        Vars.ui.hudGroup.visible(() -> state.isGame() && !GalManager.visible);
+        galGroup.fillParent = true;
+        galGroup.touchable = Touchable.childrenOnly;
+        galGroup.visible(() -> state.isGame() && GalManager.visible);
+        Core.scene.add(galGroup);
+        GalFragment.build(galGroup);
     }
 }
--- ssrc/src/cd/entities/LiquidFiltered.scala
+++
@@ -1,9 +0,0 @@
-package cd.entities
-
-import mindustry.`type`.Liquid
-import mindustry.gen.Building
-
-trait LiquidFiltered extends CDBaseBuilding{
-  override def acceptLiquid(source: Building, liquid: Liquid): Boolean =
-    super.acceptLiquid(source, liquid) && block.liquidFilter(liquid.id)
-}
